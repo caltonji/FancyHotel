d1b9e5cab321c2f280483a5bb27a2835a1a9c025
Kelcy Newton <kelcynewton@gmail.com>
2015-11-30 01:08:05 -0500

    added last of customer views

--- routeManager.js
+++ routeManager.js
@@ -44,11 +44,41 @@ module.exports = function(app, passport) {
         res.render("customer/find_room.ejs", { message: req.flash('reservationMessage') });
     });
 
-	app.get("/availablerooms", function(req,res) {
-        res.render("customer/make_reservation.ejs", { message: req.flash('reservationMessage') });
-    });
+		app.get("/availablerooms", function(req,res) {
+	        res.render("customer/make_reservation.ejs", { message: req.flash('reservationMessage') });
+	    });
 
+		app.get("/reservationdetails", function(req,res) {
+					res.render("customer/reservation_details.ejs", { message: req.flash('reservationMessage') });
+			});
 
+		app.get("/paymentinfo", function(req,res) {
+					res.render("customer/payment_info.ejs", { message: req.flash('reservationMessage') });
+			});
+
+		app.get("/updatereservation1", function(req,res) {
+					res.render("customer/update_reservation1.ejs", { message: req.flash('reservationMessage') });
+			});
+
+		app.get("/updatereservation2", function(req,res) {
+					res.render("customer/update_reservation2.ejs", { message: req.flash('reservationMessage') });
+			});
+
+		app.get("/updatereservation3", function(req,res) {
+					res.render("customer/update_reservation3.ejs", { message: req.flash('reservationMessage') });
+			});
+
+		app.get("/cancelreservation", function(req,res) {
+					res.render("customer/cancel_reservation.ejs", { message: req.flash('reservationMessage') });
+			});
+
+		app.get("/viewreview", function(req,res) {
+					res.render("customer/view_review.ejs", { message: req.flash('reservationMessage') });
+			});
+
+		app.get("/givereview", function(req,res) {
+					res.render("customer/give_review.ejs", { message: req.flash('reservationMessage') });
+			});
     //customer post routes
     app.post("/findRoom", isLoggedIn, customerRoutes.findRoom);
 
