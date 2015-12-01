0352a998dff98d5d8574bbcba907971431a39450
caltonji <caltonji@gmail.com>
2015-11-30 20:59:10 -0500

    routing and dynaminc loading for rooms

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -1,4 +1,6 @@
+var express = require('express');
 var mysql = require('mysql');
+var flash = require('connect-flash');
 
 var connection = mysql.createConnection({
   host: "academic-mysql.cc.gatech.edu",
@@ -6,15 +8,17 @@ var connection = mysql.createConnection({
   password: "tEVtJmV_",
   database : "cs4400_Group_6"
 });
+//just never let the connection
+connection.connect()
+var example = [{ Room_no : 211, Room_category : 'Suite', No_people : 4, Cost_day : 250, Cost_extra_bed_day : 150, location: "Atlanta"},
+                        { Room_no : 103, Room_category : 'Standard', No_people : 2, Cost_day : 100, Cost_extra_bed_day : 70, location: "Atlanta"}];
 
-exports.findRoom = function(req,res) {
-	connection.connect(function(err) {
-		if (err) {
-			req.flash('reservationMessage', 'Problem connecting to DB. Are you on GT wifi/a VPN?');
-			res.redirect('/newReservation');
-		}
-	});
-	connection.end();
-	console.log(connection);
-	console.log("the prev line will not be blank if connection lives everywhere");
+
+
+exports.findRooms = function(req,res) {
+	//FIXME: make actual query and return results
+	console.log("startdate: " + req.body.startDate + " end date " + req.body.endDate + " location " + req.body.location);
+	var rooms = example;
+	req.flash('rooms', rooms);
+	res.redirect('/availablerooms');
 }
