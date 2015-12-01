08a68204b7eb5751416e53cf98f2567e392c4820
Omar Mujahid <omar_mjhd@yahoo.com>
2015-12-01 13:25:41 -0500

    Updated comments on reports and changed reservation report

--- SqlStatementCreator.js
+++ SqlStatementCreator.js
@@ -81,15 +81,15 @@ exports.searchRooms = function(roomArray) { //TODO: test this?
 }
 
 exports.createReservationReport = function(month_number) { //This will need to be run once for each month
-	return "SELECT *, COUNT( * ) AS location_count FROM RESERVATION NATURAL JOIN HAS_ROOM WHERE MONTH(Start_date) = "
+	return "SELECT location, COUNT( * ) AS location_count FROM RESERVATION NATURAL JOIN HAS_ROOM WHERE MONTH(Start_date) = "
 		+ mysql.escape(month_number) + "AND Is_cancelled = " + mysql.escape(0) + " GROUP BY location";
-}
+} //Returns tuples in the form (location, location_count)
 
 exports.createPopularRoomReport = function(month_number) { //This will need to be run once for each month
 	return "SELECT location, Room_category, MAX(sub.counter)FROM (SELECT location, Room_category, COUNT( * ) AS counter"
 		+ " FROM ROOM NATURAL JOIN RESERVATION NATURAL JOIN HAS_ROOM WHERE MONTH( Start_date ) =  " + mysql.escape(month_number)
 		+ "AND Is_cancelled = " + mysql.escape(0) + " GROUP BY location, Room_category) AS sub GROUP BY location;";
-}
+} //Returns tuples in the form (location, Room_category, count)
 
 
 
