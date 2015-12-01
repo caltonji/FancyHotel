11038882d5f242bccd8ded2511321c07187df0fd
Omar Mujahid <omar_mjhd@yahoo.com>
2015-12-01 13:19:19 -0500

    Added Query to create Popular Room Report

--- SqlStatementCreator.js
+++ SqlStatementCreator.js
@@ -80,9 +80,15 @@ exports.searchRooms = function(roomArray) { //TODO: test this?
 	return query;
 }
 
-exports.createReservationReport = function(month_number) {
+exports.createReservationReport = function(month_number) { //This will need to be run once for each month
 	return "SELECT *, COUNT( * ) AS location_count FROM RESERVATION NATURAL JOIN HAS_ROOM WHERE MONTH(Start_date) = "
 		+ mysql.escape(month_number) + "AND Is_cancelled = " + mysql.escape(0) + " GROUP BY location";
+}
+
+exports.createPopularRoomReport = function(month_number) { //This will need to be run once for each month
+	return "SELECT location, Room_category, MAX(sub.counter)FROM (SELECT location, Room_category, COUNT( * ) AS counter"
+		+ " FROM ROOM NATURAL JOIN RESERVATION NATURAL JOIN HAS_ROOM WHERE MONTH( Start_date ) =  " + mysql.escape(month_number)
+		+ "AND Is_cancelled = " + mysql.escape(0) + " GROUP BY location, Room_category) AS sub GROUP BY location;";
 }
 
 
